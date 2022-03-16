4f1815af923aa8c325d486111d380beabba7c82c
Parrael <raphaelph2005@gmail.com>
2022-03-16 17:12:17 -0300

    Exercicios da prova diagnóstica.

---
+++ ProvaDiagnostica/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProvaDiagnostica
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solucao ProvaDiag = new Solucao();
+            Console.WriteLine("Prova Diagnóstica");
+            //ProvaDiag.Ex1();
+            ProvaDiag.Ex2();
+
+        }
+    }
+}

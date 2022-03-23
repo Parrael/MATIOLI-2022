e08601a9f5739d79f14df3d69c62b5da35f4a898
Parrael <raphaelph2005@gmail.com>
2022-03-23 17:15:14 -0300

    Feito por Raphael Parra e Isabela SIlvestre

--- Estrutura Sequencial certo/Solucao/EstruturaSequencial/Program.cs
+++ Estrutura Sequencial certo/Solucao/EstruturaSequencial/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 
-namespace exercEstruturaSequencial
+namespace EstruturaSequencial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Solucao ExSeq = new Solucao();
-            ExSeq.ex1();
+            Resolucoes ExSeq = new Resolucoes();
+            //ExSeq.ex1();
             //ExSeq.ex2();
             //ExSeq.ex3();
             //ExSeq.ex4();
             //ExSeq.ex5();
-            //ExSeq.ex6();
+            ExSeq.ex6();
+            
         }
     }
 }

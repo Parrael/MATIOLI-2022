4fbe4c8a9e4776b2f0b4aaa8c8cd81e3ff2bdbca
Parrael <raphaelph2005@gmail.com>
2022-03-30 17:30:23 -0300

    Exercicio_Form_Calculadora

---
+++ Windows_Form/Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            if (!txtDisplay.Text.Trim().Equals("0"))
+            {
+                txtDisplay.Text = txtDisplay.Text + "0";
+            }
+            else
+            {
+                txtDisplay.Text = "0";
+            }
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text += "1";
+        }
+    }
+
+}

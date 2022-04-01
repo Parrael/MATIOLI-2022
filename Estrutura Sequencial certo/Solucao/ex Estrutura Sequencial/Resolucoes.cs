08a2e5c85c5a72a7fcbd32ac4f516d28c9d90154
Parrael <cl201286@g.unicamp.br>
2022-04-01 20:29:51 -0300

    ex4

--- Estrutura Sequencial certo/Solucao/ex Estrutura Sequencial/Resolucoes.cs
+++ Estrutura Sequencial certo/Solucao/ex Estrutura Sequencial/Resolucoes.cs
@@ -6,18 +6,35 @@ namespace EstruturaSequencial
 {
     class Resolucoes
     {
-        public void ex2()
+        public void ex5()
         {
-            Console.WriteLine("\n\n\t\tEXERCÍCIO 2\n\n");
+            Console.WriteLine("\n\n\t\tEXERCÍCIO 5\n\n");
+            int codPec1, codPec2;                //código da peça 1 e da 2
+            int quantPec1, quantPec2;               //quantidade de peças 1 e 2 
+            float vUnit1, vUnit2;                   //valor peça 1 e 2
+            float tProd1, tProd2, valorTotal;       //valor total de cada produto / compra final  
 
-            double n1, pi;
-            Console.WriteLine("\nInsira o raio do seu círculo: ");
-            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nInsira o código do produto 1: ");
+            codPec1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInsira a quantidade que será comprada deste produto: ");
+            quantPec1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInforme o valor unitário deste produto: ");
+            vUnit1 = float.Parse(Console.ReadLine());
 
-            pi = 3.14159;
-            n1 = pi * n1 * n1;
 
-            Console.WriteLine("\nA área desse cículo será de: {0:F4}", n1);
+            Console.WriteLine("\nInsira o código do produto 2: ");
+            codPec2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInsira a quantidade que será comprada deste produto: ");
+            quantPec2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInforme o valor unitário deste produto: ");
+            vUnit2 = float.Parse(Console.ReadLine());
+
+            tProd1 = quantPec1 * vUnit1;
+            tProd2 = quantPec2 * vUnit2;
+
+            valorTotal = tProd1 + tProd2;
+
+            Console.WriteLine("\nO valor total da sua compra é de R${0:F2}", valorTotal);
         }
     }
 }

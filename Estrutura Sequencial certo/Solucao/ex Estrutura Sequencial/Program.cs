6d924c68bba4b65b0d9aa5a6c817c00d1f93239a
Parrael <cl201286@g.unicamp.br>
2022-04-01 20:14:21 -0300

    ex2

---
+++ Estrutura Sequencial certo/Solucao/ex Estrutura Sequencial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resolucoes ExSeq = new Resolucoes();
+            
+            ExSeq.ex2();
+
+        }
+    }
+}
---
+++ Estrutura Sequencial certo/Solucao/ex Estrutura Sequencial/Resolucoes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstruturaSequencial
+{
+    class Resolucoes
+    {
+        public void ex2()
+        {
+            Console.WriteLine("\n\n\t\tEXERCÍCIO 2\n\n");
+
+            double n1, pi;
+            Console.WriteLine("\nInsira o raio do seu círculo: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            pi = 3.14159;
+            n1 = pi * n1 * n1;
+
+            Console.WriteLine("\nA área desse cículo será de: {0:F4}", n1);
+        }
+    }
+}

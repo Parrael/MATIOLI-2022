3f0ea43e56cc83e79d7462af88353df83c26eb94
Parrael <cl201286@g.unicamp.br>
2022-04-01 19:49:50 -0300

    Branch Ex1

---
+++ Estrutura Sequencial certo/SolucaoEx1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resolucoes ExSeq = new Resolucoes();
+            ExSeq.ex1();
+
+        }
+    }
+}
